6216db2d9ba730e5b0f87239fae75ac41add3f4a
Karl J. Overå <k@rl.run>
2018-10-29 19:33:58 +0100

    Add a basic http server

--- src/now_nim_serverless.nim
+++ src/now_nim_serverless.nim
@@ -1,1 +1,13 @@
-echo "Hello World!"
+import asynchttpserver, asyncdispatch, json
+
+var server = newAsyncHttpServer()
+proc cb(req: Request) {.async.} =
+  let headers = newHttpHeaders([("Content-Type","application/json")])
+  if req.url.path == "/secret":
+    let msg = %* {"message": "Welcome to the secret"}
+    await req.respond(Http200, $msg, headers)
+  else:
+    let msg = %* {"message": "Hello path: " & req.url.path}
+    await req.respond(Http200, $msg, headers)
+
+waitFor server.serve(Port(5000), cb)
